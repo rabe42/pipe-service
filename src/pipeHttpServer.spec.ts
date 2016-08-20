10a35ab55eb0161d74759ebc3e2c83450a06c37d
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2016-08-20 14:25:15 +0200

    Introduction of a singleton pattern.

--- src/pipeHttpServer.spec.ts
+++ src/pipeHttpServer.spec.ts
@@ -1,16 +1,20 @@
-import {PipeHttpServer} from "./pipeHttpServer";
+import * as http from "http";
+import * as querystring from "querystring";
+
+import {PipeHttpServer, pipeHttpServerSingleton} from "./pipeHttpServer";
 
 describe("The pipe http server", () => {
 
-    var httpServer: PipeHttpServer;
-
-    it("should be possible to be created.", () => {
-        httpServer = new PipeHttpServer();
-        expect(httpServer).toBeDefined();
+    it("should not be possible to be a 2nd instance.", () => {
+        try {
+            var httpServer = new PipeHttpServer();
+            fail("2nd instance created.");
+        }
+        catch (e) {};
     });
     it("should start the server", (done) => {
         try {
-            httpServer.start((err) => {
+            pipeHttpServerSingleton.start((err) => {
                 if (err) {
                     fail("Wasn't able to start server due to: " + err);
                 }
@@ -21,7 +25,29 @@ describe("The pipe http server", () => {
             fail("Wasn't able to start the server as I catched: " + error);
         }
     });
+    it("should accept requests", (done) => {
+        let message = querystring.stringify({msg: "Hello Pipe", attribute: "attribute1", paramenter: "parameter1"});
+        let clientRequest = http.request({hostname: "localhost", port: 8081, method: "PUT", path: "/hello"},
+            (result: http.IncomingMessage) => {
+                result.setEncoding('utf8');
+                result.on('end', () => { 
+                    expect(result.statusCode).toBe(200);
+                    done();
+                 });
+            });
+        clientRequest.on("error", (err: any) => {
+            fail("Request failed due to: " + err);
+        });
+        clientRequest.on("end", (err: any, result: http.IncomingMessage) => {
+            expect(result).toBeDefined();
+            expect(result.statusCode).toBeDefined();
+            expect(result.statusCode).toBe(200);
+            done();
+        });
+        clientRequest.write(message);
+        clientRequest.end();
+    });
     it("should terminate the server", () => {
-        httpServer.close();
+        pipeHttpServerSingleton.close();
     });
 });
--- src/pipeHttpServer.ts
+++ src/pipeHttpServer.ts
@@ -7,22 +7,47 @@ import {pipeHttpServerLoggerConfig} from "./loggerConfig";
 
 var logger = bunyan.createLogger(pipeHttpServerLoggerConfig);
 
+/**
+ * This is the http server, which accepts messages for certain pipes.
+ * 
+ * TODO Read pipe configuration
+ * TODO Create server response
+ */
 export class PipeHttpServer {
-
     server: any;
-    port: number = 8080;
+    port: number = 8081;
     hostname: string = "localhost";
 
     constructor() {
-        this.server = http.createServer(this.serve); 
+        if (pipeHttpServerSingleton) {
+            throw Error("It's not allowed to create a second instance.")
+        }
     }
 
-    private serve(req: any, res: any): void {
+    /**
+     * This is the central entry point for the management of the http request.
+     * It distributes the requests to the different other methods "put", "get", "delete", "post".
+     */
+    public serve(req: http.IncomingMessage, res: http.ServerResponse): void {
         logger.info("PipeHttpServer.serve(): Service called.");
+        if (req.method === "PUT") {
+            this.put(req, res);
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("Hello from the pipe world")
+        }
+    }
+
+    private put(request: http.IncomingMessage, response: http.ServerResponse): void {
+        logger.info("PipeHttpServer.put(): Service called.")
+        response.statusCode = 200;
+        response.end("end");
     }
 
     public start(callback: (err: any) => void) {
-        this.server.listen(this.port, this.hostname, (err: any) => {
+        server.listen(this.port, this.hostname, (err: any) => {
             if (err) {
                 logger.error("PipeHttpServer.start(): Couldn't start server due to: " + err);
             }
@@ -32,6 +57,16 @@ export class PipeHttpServer {
     }
 
     public close() {
-        this.server.close();
+        server.close();
     }
-}
+};
+
+export const pipeHttpServerSingleton: PipeHttpServer = new PipeHttpServer();
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
+    if (!pipeHttpServerSingleton) {
+        logger.error("PipeHttpServer::createServer: No pipe server created!");
+        throw Error("PipeHttpServer singleton must be initialized first!");
+    }
+    pipeHttpServerSingleton.serve(request, response);
+});
