a1b05810abdaa7a6bb2809bb3f78a08dfa71cf3e
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2016-08-27 12:21:24 +0200

    Documentation of the trick to overcome a JS/TS model flaw.

--- src/http-integration-tests/http.spec.ts
+++ src/http-integration-tests/http.spec.ts
@@ -20,9 +20,9 @@ describe("http service", () => {
     });
     server2.listen(8082, "localhost");
 
-    function request(port: number, expectedResponse: string, expectedStatus: number, callback: () => void): void {
+    function request(method: string, port: number, expectedResponse: string, expectedStatus: number, callback: () => void): void {
         var responseConent = "";
-        let clientRequest = http.request({hostname: "localhost", port: port, method: "PUT", path: "/"},
+        let clientRequest = http.request({hostname: "localhost", port: port, method: method, path: "/"},
             (result: http.IncomingMessage) => {
                 result.setEncoding('utf8');
                 result.on('data', (chunk: any) => {
@@ -40,15 +40,67 @@ describe("http service", () => {
         clientRequest.end();
     };
 
+    class MyServer {
+        port: number;
+        server: any;
+
+        constructor(port: number) {
+            this.port = port;
+            this.server = http.createServer(this.serve);
+            // Adding the parent to the server instace, provides access to this object later on.
+            this.server.parent = this;
+            this.server.listen(this.port, "localhost");
+        }
+        /** 
+         * This is an alien!
+         */
+        private serve(request: http.IncomingMessage, response: http.ServerResponse) {
+            // Casting the this to the parent object, as this will be the server at the point, the method is called.
+            if (request.method === "GET") {
+                (<any>this).parent.get(request, response);
+            }
+            else if (request.method === "PUT") {
+                (<any>this).parent.put(request, response);
+            }
+            else if (request.method === "POST") {
+                (<any>this).parent.post(request, response);
+            }
+            else if (request.method === "DELETE") {
+                (<any>this).parent.delete(request, response);
+            }
+        }
+        get(request: http.IncomingMessage, response: http.ServerResponse) {
+            response.statusCode = 200;
+            response.end("GET processed! (" + this.port + ")");
+        }
+        put(request: http.IncomingMessage, response: http.ServerResponse) {
+            response.statusCode = 200;
+            response.end("PUT processed! (" + this.port + ")");
+        }
+        post(request: http.IncomingMessage, response: http.ServerResponse) {
+            response.statusCode = 200;
+            response.end("POST processed! (" + this.port + ")");
+        }
+        delete(request: http.IncomingMessage, response: http.ServerResponse) {
+            response.statusCode = 200;
+            response.end("DELETE processed! (" + this.port + ")");
+        }
+        close() {
+            this.server.close();
+        }
+    }
+    var myserver1: MyServer = new MyServer(8083);
+    var myserver2: MyServer = new MyServer(8084);
+
     it("should be possible to request server1 only.", (done) => {
-        request(8081, "Ok1", 200, done);
+        request("GET", 8081, "Ok1", 200, done);
     });
     it("should have been really called 1st service.", () => {
         expect(server1Request).toBeDefined();
         expect(server2Request).toBeUndefined();
     });
     it("should be possible to request server2 only", (done) => {
-        request(8082, "Ok2", 200, done);
+        request("PUT", 8082, "Ok2", 200, done);
     });
     it("should have been really called 2nd service.", () => {
         expect(server1Request).toBeDefined();
@@ -57,5 +109,15 @@ describe("http service", () => {
     it("should be possible to close the services.", () => {
         server1.close();
         server2.close();
+    });
+    it("should be possible to use an instance context of a class.", (done) => {
+        request("GET", 8084, "GET processed! (8084)", 200, done);
+    });
+    it("should be possible to use also the other instance.", (done) => {
+        request("PUT", 8083, "PUT processed! (8083)", 200, done);
+    })
+    it("should be possible to close my servers.", () => {
+        myserver1.close();
+        myserver2.close();
     });
 });
